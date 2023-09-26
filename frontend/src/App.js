04eb3293a8cd459e267877de4dbecd562ddbde93
Kenny Brown <knbrown6@asu.edu>
2023-09-25 17:21:10 -0700

    Update app

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,17 +1,24 @@
-import React from 'react';
-import './App.css';
-import { MapContainer, TileLayer } from 'react-leaflet';
-import 'leaflet/dist/leaflet.css';
+import React from "react";
+import "./App.css";
+import { MapContainer, TileLayer } from "react-leaflet";
+import "leaflet/dist/leaflet.css";
 
 function App() {
   return (
-    <div className="leaflet-container">
-      <MapContainer center={[51.505, -0.09]} zoom={13} style={{ height: "100%", width: "100%" }}>
-        <TileLayer
-          url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
-          attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
-        />
-      </MapContainer>
+    <div className="container">
+      <div className="header">RadarRewind</div>
+      <div className="leaflet-container">
+        <MapContainer
+          center={[37.60900565748196, -122.32823527245013]}
+          zoom={11}
+          style={{ height: "100%", width: "100%" }}
+        >
+          <TileLayer
+            url="https://{s}.basemaps.cartocdn.com/light_all/{z}/{x}/{y}.png"
+            attribution='&copy; <a href="https://www.carto.com/">CARTO</a> | &copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+          />
+        </MapContainer>
+      </div>
     </div>
   );
 }
