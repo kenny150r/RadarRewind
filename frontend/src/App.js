49ad763a44afe39421c7dcd9975c60a62b995856
Kenny Brown <knbrown6@asu.edu>
2023-10-03 22:06:26 -0700

    Add stations

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -2,12 +2,21 @@ import React from "react";
 import "./App.css";
 import { MapContainer, TileLayer } from "react-leaflet";
 import "leaflet/dist/leaflet.css";
+import DateTimeOverlay from './components/DateTimeOverlay';
 
 function App() {
+
+  const handleDateTimeSubmit = (date, time) => {
+    console.log("Selected Date:", date);
+    console.log("Selected Time:", time);
+    // Pass the data to another function or perform your desired action
+  };
+
   return (
     <div className="container">
       <div className="header">RadarRewind</div>
       <div className="leaflet-container">
+        <DateTimeOverlay onSubmit={handleDateTimeSubmit} />
         <MapContainer
           center={[37.60900565748196, -122.32823527245013]}
           zoom={11}
---
+++ frontend/src/components/DateTimeOverlay.js
@@ -0,0 +1,34 @@
+import React from "react";
+import stations from '../data/nexrad_station_id.json';
+
+function DateTimeOverlay({ onSubmit }) {
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    const date = e.target.date.value;
+    const time = e.target.time.value;
+    const station = e.target.station.value;
+    onSubmit(date, time, station);
+  };
+
+  return (
+    <div className="timeoverlay">
+      <div className="timeoverlayhead">
+        <h1>Let's Rewind!</h1>
+      </div>
+      <form onSubmit={handleSubmit}>
+        <label>
+           <select name="station" required>
+            {stations.map(station => (
+                <option key={station.ID} value={station.ID}>{station.ID}</option>
+              ))}
+           </select>
+         </label>
+        <input type="date" name="date" required />
+        <input type="time" name="time" required />
+        <button type="submit">Go</button>
+      </form>
+    </div>
+  );
+}
+
+export default DateTimeOverlay;
